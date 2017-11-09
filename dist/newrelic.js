53bba041a00c93ee545d4f849248606249f546db
xun <xunding@gmail.com>
2017-11-09 12:13:27 -0600

    add newlic

---
+++ dist/newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['bitcoin-news-chart'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '6d195f01fd124384f95a3ebf11eac26ab1f1a092',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
